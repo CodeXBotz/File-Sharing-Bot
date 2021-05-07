5c28034fdc025c3e4c26e2f1202967d7ec7a343b
ERIC-DANIYEL <73272797+ERICDANIYEL@users.noreply.github.com>
2021-05-07 09:41:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 import logging
 from logging.handlers import RotatingFileHandler
-
+#Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-
+#Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", ""))
-
+#Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
-
+#Your channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-
+#OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-
+#start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {firstname}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
