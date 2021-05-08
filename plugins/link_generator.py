5bc0c88072354ba694cdeba2ef5fd7b48febbfe7
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-05-09 02:41:18 +0530

    Debug

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -37,7 +37,7 @@ async def batch(client: Client, message: Message):
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
-async def batch(client: Client, message: Message):
+async def link_generator(client: Client, message: Message):
     while True:
         try:
             channel_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..", chat_id = message.from_user.id, filters=filters.forwarded, timeout=30)
