2cf32e3236b2285ca9a0b84f51ca26a4f72f23ad
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-05-09 02:57:29 +0530

    Debug

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -50,7 +50,7 @@ async def link_generator(client: Client, message: Message):
         await channel_message.reply_text("Forward from the Assigned Channel only...", quote = True)
         continue
 
-    base64_string = await encode("get-{msg_id}")
+    base64_string = await encode(f"get-{msg_id}")
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
