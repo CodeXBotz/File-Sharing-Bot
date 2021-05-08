d71bc13302e25bd0efb8c71b371022df4a179d3d
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-05-09 01:58:37 +0530

    Fixed bug in Encode Calling

--- plugins/commands.py
+++ plugins/commands.py
@@ -144,7 +144,7 @@ async def batch(client: Client, message: Message):
         await second_message.reply_text("Forward from the Assigned Channel only...", quote = True)
         continue
     string = f"get-{f_msg_id}-{s_msg_id}"
-    base64_string = await decode(string)
+    base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
