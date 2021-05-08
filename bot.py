f5966409ba56a37d409f8ad073d99d2e1d397df9
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-05-08 19:50:19 +0530

    Added Invite link generation in Client for Force Sub Channel

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import pyromod.listen
 from pyrogram import Client
+import sys
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL
 
 class Bot(Client):
     def __init__(self):
@@ -20,6 +21,17 @@ class Bot(Client):
     async def start(self):
         await super().start()
         usr_bot_me = await self.get_me()
+
+        if FORCE_SUB_CHANNEL:
+            try:
+                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                self.invitelink = link
+            except:
+                self.LOGGER(__name__).warning("I can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning("Please Double check the FORCE_SUB_CHANNEL value and Make sure I am Admin in channel with Invite Users via Link Permission")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                sys.exit()
+
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾𝙤𝙙𝙚 𝕏 𝘽𝙤𝙩𝙯\nhttps://t.me/CodeXBotz")
         self.username = usr_bot_me.username
