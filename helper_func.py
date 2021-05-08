974b14d3c3accd56454c7bc29641d8929a49c605
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-05-09 01:49:21 +0530

    Added encode and decode function

--- helper_func.py
+++ helper_func.py
@@ -1,3 +1,4 @@
+import base64
 from pyrogram import filters
 from config import FORCE_SUB_CHANNEL, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
@@ -17,5 +18,17 @@ async def is_subscribed(filter, client, message):
         return False
     else:
         return True
+
+async def encode(string):
+    string_bytes = string.encode("ascii")
+    base64_bytes = base64.b64encode(string_bytes)
+    base64_string = base64_bytes.decode("ascii")
+    return base64_string
+
+async def decode(base64_string):
+    base64_bytes = base64_string.encode("ascii")
+    string_bytes = base64.b64decode(base64_bytes) 
+    string = string_bytes.decode("ascii")
+    return string
 
 subscribed = filters.create(is_subscribed)
