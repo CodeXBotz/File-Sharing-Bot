33c7e755757be74d7fb5cc1237cd6eeeceec0367
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-05-09 06:09:55 +0530

    Removed CHANNEL_ID variable

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
-from config import CHANNEL_ID, ADMINS
+from config import ADMINS
 from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
@@ -12,7 +12,7 @@ async def batch(client: Client, message: Message):
         except:
             return
         if first_message.forward_from_chat:
-            if first_message.forward_from_chat.id == CHANNEL_ID:
+            if first_message.forward_from_chat.id == client.db_channel.id:
                 f_msg_id = first_message.forward_from_message_id
                 break
         await first_message.reply_text("Forward from the Assigned Channel only...", quote = True)
@@ -23,13 +23,13 @@ async def batch(client: Client, message: Message):
         except:
             return
         if second_message.forward_from_chat:
-            if second_message.forward_from_chat.id == CHANNEL_ID:
+            if second_message.forward_from_chat.id == client.db_channel.id:
                 s_msg_id = second_message.forward_from_message_id
                 break
         await second_message.reply_text("Forward from the Assigned Channel only...", quote = True)
         continue
 
-    string = f"get-{f_msg_id * abs(CHANNEL_ID)}-{s_msg_id * abs(CHANNEL_ID)}"
+    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
@@ -44,13 +44,13 @@ async def link_generator(client: Client, message: Message):
         except:
             return
         if channel_message.forward_from_chat:
-            if channel_message.forward_from_chat.id == CHANNEL_ID:
+            if channel_message.forward_from_chat.id == client.db_channel.id:
                 msg_id = channel_message.forward_from_message_id
                 break
         await channel_message.reply_text("Forward from the Assigned Channel only...", quote = True)
         continue
 
-    base64_string = await encode(f"get-{msg_id * abs(CHANNEL_ID)}")
+    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
