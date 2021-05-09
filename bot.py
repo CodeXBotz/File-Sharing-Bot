7216261317b576fa9d4e9363c07c9232986e8bbb
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-05-09 08:10:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,6 +36,8 @@ class Bot(Client):
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning("Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value")
