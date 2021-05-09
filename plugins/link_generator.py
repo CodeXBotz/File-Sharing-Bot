471b5086b365ae110148cd31df50060561354f44
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-05-09 06:52:05 +0530

    Added Link option when batch command works

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,3 +1,4 @@
+import re
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
@@ -8,26 +9,66 @@ from helper_func import encode
 async def batch(client: Client, message: Message):
     while True:
         try:
-            first_message = await client.ask(text = "Forward the First Message from the DB Channel (with Quotes)..", chat_id = message.from_user.id, filters=filters.forwarded, timeout=30)
+            first_message = await client.ask(text = "Forward the First Message from DB Channel (with Quotes)..\nor Send the Post Link", chat_id = message.from_user.id, filters=filters.forwarded, timeout=30)
         except:
             return
         if first_message.forward_from_chat:
             if first_message.forward_from_chat.id == client.db_channel.id:
-                f_msg_id = first_message.forward_from_message_id
+                s_msg_id = first_message.forward_from_message_id
                 break
-        await first_message.reply_text("Forward from the Assigned Channel only...", quote = True)
-        continue
+            await first_message.reply_text("Forward from the Assigned Channel only...", quote = True)
+            continue
+        elif first_message.forward_sender_name:
+            await second_message.reply_text("Forward from the Assigned Channel only...", quote = True)
+            continue
+        elif first_message.text:
+            pattern = "https://t.me/(?:c/)?(.*)/(\d+)"
+            matches = re.match(pattern,first_message.text)
+            if not matches:
+                await first_message.reply("This is not a Proper telegram post link", quote=True)
+                continue
+            channel_id = matches.group(1)
+            s_msg_id = matches.group(2)
+            if channel_id.isdigit():
+                if channel_id == client.db_channel.id
+                    break
+            else:
+                if channel_id == client.db_channel.username
+                    break
+            await first_message.reply("Send the link of post from the db channel only", quote=True)
+            continue
+
     while True:
         try:
-            second_message = await client.ask(text = "Forward the Last Message from DB Channel (with Quotes)..", chat_id = message.from_user.id, filters=filters.forwarded, timeout=30)
+            second_message = await client.ask(text = "Forward the Last Message from DB Channel (with Quotes)..\nor Send the Post Link", chat_id = message.from_user.id, filters=filters.forwarded, timeout=30)
         except:
             return
         if second_message.forward_from_chat:
             if second_message.forward_from_chat.id == client.db_channel.id:
                 s_msg_id = second_message.forward_from_message_id
                 break
-        await second_message.reply_text("Forward from the Assigned Channel only...", quote = True)
-        continue
+            await second_message.reply_text("Forward from the Assigned Channel only...", quote = True)
+            continue
+        elif second_message.forward_sender_name:
+            await second_message.reply_text("Forward from the Assigned Channel only...", quote = True)
+            continue
+        elif second_message.text:
+            pattern = "https://t.me/(?:c/)?(.*)/(\d+)"
+            matches = re.match(pattern,text)
+            if not matches:
+                await second_message.reply("This is not a Proper telegram post link", quote=True)
+                continue
+            channel_id = matches.group(1)
+            s_msg_id = matches.group(2)
+            if channel_id.isdigit():
+                if channel_id == client.db_channel.id
+                    break
+            else:
+                if channel_id == client.db_channel.username
+                    break
+            await second_message.reply("Send the link of post from the db channel only", quote=True)
+            continue
+
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
