f99eef51caf7be09587c0b41cab1295723ef2507
Sʜᴀʜsᴀᴅ Kᴏʟᴀᴛʜᴜʀ <shahsadkpklr@gmail.com>
2021-05-17 13:04:19 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -65,7 +65,7 @@ async def start_command(client: Client, message: Message):
                 await asyncio.sleep(0.5)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html' reply_markup = reply_markup)
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
             except:
                 pass
         return
