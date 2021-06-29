c2136cf7e516d353ee48f15a7a32545e81940c24
lifendry <30863636+lifendry@users.noreply.github.com>
2021-06-29 13:15:34 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Pemilik Chanel : <a href='tg://user?id={OWNER_ID}'>pentil kuda</a>\n○ Language : <code>Python3</code>\n○ Channel : <a href='https://t.me/Jhonny_perkasa'>@Jhonny_perkasa</a>\n○ BANTU SUPPORT <a href='https://t.me/Jhonny_perkasa'>@Jhonny_perkasa</a>BIAR BACOL MAKIN BANYAK</b></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
