5b80fea628f8b8eb4cf1370236d327b7d29e3e6a
Thiyaku17 <65347563+Thiyaku17@users.noreply.github.com>
2021-07-10 18:40:15 +0530

    Create presets.py

---
+++ presets.py
@@ -0,0 +1,10 @@
+class Presets(object):
+    WELCOME_MESSAGE = """
+<b>Hello.. </b>{}
+<code>I'm an official File Sharing bot
+maintained by</code> <b>{}</b>
+<code>Keep me unblocked to receive ma official updates.</code>
+    """
+    USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
+    WAIT_MSG = "<b>Please Wait...</b>"
+    REPLY_ERROR = "<code>Use this command as a replay to any telegram message with out any spaces.</code>"
