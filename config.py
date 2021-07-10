77ebd2fbed70241778684e90af9ff50ec3369771
Thiyaku17 <65347563+Thiyaku17@users.noreply.github.com>
2021-07-10 18:43:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -63,3 +63,36 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+
+import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+class Config(object):
+    # Get a bot token from botfather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+    # Get from my.telegram.org
+    APP_ID = int(os.environ.get("APP_ID", ""))
+
+    # Get from my.telegram.org
+    API_HASH = os.environ.get("API_HASH", "")
+
+    # Database URI
+    DB_URI = os.environ.get("DATABASE_URL", "")
+
+    # Group / channel Id: Members to which inline query answered
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "")
+
+    # List of admin user ids for special functions(Storing as an array)
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
