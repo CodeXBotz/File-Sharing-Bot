672219eee008bff99aff115571348c532e59d6f5
Thiyaku17 <65347563+Thiyaku17@users.noreply.github.com>
2021-07-10 18:33:41 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,100 +1,122 @@
-(c) @AbirHasan2005
 
-import time
-import string
-import random
+import os
+
 import asyncio
-import datetime
-import aiofiles.os
-import traceback
-from configs import Config
-from helpers.database.access_db import db
+
+from presets import Presets
+
 from pyrogram.types import Message
-from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
-broadcast_ids = {}
+from pyrogram import Client, filters
 
+from pyrogram.errors import FloodWait
 
-async def send_msg(user_id, message):
-    try:
-        if Config.BROADCAST_AS_COPY is False:
-            await message.forward(chat_id=user_id)
-        elif Config.BROADCAST_AS_COPY is True:
-            await message.copy(chat_id=user_id)
-        return 200, None
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return send_msg(user_id, message)
-    except InputUserDeactivated:
-        return 400, f"{user_id} : deactivated\n"
-    except UserIsBlocked:
-        return 400, f"{user_id} : blocked the bot\n"
-    except PeerIdInvalid:
-        return 400, f"{user_id} : user id invalid\n"
-    except Exception as e:
-        return 500, f"{user_id} : {traceback.format_exc()}\n"
+from library.support import users_info
 
+from library.sql import add_user, query_msg
 
-async def broadcast_handler(m: Message):
-    all_users = await db.get_all_users()
-    broadcast_msg = m.reply_to_message
-    while True:
-        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
-        if not broadcast_ids.get(broadcast_id):
-            break
-    out = await m.reply_text(
-        text=f"Broadcast Started! You will be notified with log file when all the users are notified."
-    )
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    done = 0
-    failed = 0
-    success = 0
-    broadcast_ids[broadcast_id] = dict(
-        total=total_users,
-        current=done,
-        failed=failed,
-        success=success
-    )
-    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-        async for user in all_users:
-            sts, msg = await send_msg(
-                user_id=int(user['id']),
-                message=broadcast_msg
-            )
-            if msg is not None:
-                await broadcast_log_file.write(msg)
-            if sts == 200:
-                success += 1
-            else:
-                failed += 1
-            if sts == 400:
-                await db.delete_user(user['id'])
-            done += 1
-            if broadcast_ids.get(broadcast_id) is None:
-                break
-            else:
-                broadcast_ids[broadcast_id].update(
-                    dict(
-                        current=done,
-                        failed=failed,
-                        success=success
-                    )
+if bool(os.environ.get("ENV", False)):
+
+    from sample_config import Config
+
+else:
+
+    from config import Config
+
+# ------------------------------- Start Message --------------------------------- #
+
+@Client.on_message(filters.private & filters.command('start'))
+
+async def start_bot(bot, m: Message):
+
+    id = m.from_user.id
+
+    user_name = '@' + m.from_user.username if m.from_user.username else None
+
+    await add_user(id, user_name)
+
+    await m.reply_text(Presets.WELCOME_MESSAGE.format(m.from_user.mention(),
+
+                                                      Config.SUPPORT_CHAT if Config.SUPPORT_CHAT else "_______"),
+
+                       parse_mode='html',
+
+                       disable_web_page_preview=True
+
+                       )
+
+# ------------------------------- View Subscribers --------------------------------- #
+
+@Client.on_message(filters.private & filters.command('subscribers'))
+
+async def subscribers_count(bot, m: Message):
+
+    id = m.from_user.id
+
+    if id not in Config.AUTH_USERS:
+
+        return
+
+    msg = await m.reply_text(Presets.WAIT_MSG)
+
+    messages = await users_info(bot)
+
+    active = messages[0]
+
+    blocked = messages[1]
+
+    await m.delete()
+
+    await msg.edit(Presets.USERS_LIST.format(active, blocked))
+
+# ------------------------ Send messages to subs ----------------------------- #
+
+@Client.on_message(filters.private & filters.command('send'))
+
+async def send_text(bot, m: Message):
+
+    id = m.from_user.id
+
+    if id not in Config.AUTH_USERS:
+
+        return
+
+    if (" " not in m.text) and ("send" in m.text) and (m.reply_to_message is not None):
+
+        query = await query_msg()
+
+        for row in query:
+
+            chat_id = int(row[0])
+
+            try:
+
+                await bot.copy_message(
+
+                    chat_id=chat_id,
+
+                    from_chat_id=m.chat.id,
+
+                    message_id=m.reply_to_message.message_id,
+
+                    caption=m.caption,
+
+                    reply_markup=m.reply_markup
+
                 )
-    if broadcast_ids.get(broadcast_id):
-        broadcast_ids.pop(broadcast_id)
-    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await asyncio.sleep(3)
-    await out.delete()
-    if failed == 0:
-        await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
+
+            except FloodWait as e:
+
+                await asyncio.sleep(e.x)
+
+            except Exception:
+
+                pass
+
     else:
-        await m.reply_document(
-            document='broadcast.txt',
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    await aiofiles.os.remove('broadcast.txt')
+
+        msg = await m.reply_text(Presets.REPLY_ERROR, m.message_id)
+
+        await asyncio.sleep(8)
+
+        await msg.delete()
