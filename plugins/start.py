52a5d0c6a366f34e84ff06ebc08b09ed6d03efb0
FILELANGKATELE <88260364+FILELANGKATELE@users.noreply.github.com>
2021-08-17 20:54:40 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,11 +94,11 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
+    text = "<b>etss😁, sebelum liat videonya wajib gabung dulu ke channelku yang lain ya ka😉</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
+        text = text + f" <b>kalau uda klik emote ini <a href='https://t.me/{client.username}?start={argument}'>🥳🥳🥳🥳</a></b>"
     except ValueError:
         pass
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
