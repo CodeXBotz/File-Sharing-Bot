45eaca82f3bd13d75a9a6ac4100ad670daa09eef
FILELANGKATELE <88260364+FILELANGKATELE@users.noreply.github.com>
2021-08-17 23:00:24 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b> Creator : <a href='tg://user?id={OWNER_ID}'>bandar video telegram</a></b>",
+            text = f"<b> Creator : @bandar video telegram</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
