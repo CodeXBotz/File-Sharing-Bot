8b9b7e3a16588b85ff9bbc01e21b12605ebede57
FILELANGKATELE <88260364+FILELANGKATELE@users.noreply.github.com>
2021-08-17 22:33:42 +0700

    gh
    ff

--- plugins/start.py
+++ plugins/start.py
@@ -98,7 +98,7 @@ async def not_joined(client: Client, message: Message):
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>sudah join? klik disini</a></b>"
+        text = text + f" <b>dan <a href='https://t.me/{client.username}?start={argument}'>coba lagi</a></b>"
     except ValueError:
         pass
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("JOIN CHANNEL", url = client.invitelink)]])
