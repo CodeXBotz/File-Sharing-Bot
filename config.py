114dee040165bc834399846c64a3e3c81fcafbe6
FILELANGKATELE <88260364+FILELANGKATELE@users.noreply.github.com>
2021-08-17 21:24:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-#Bot token @Botfather
+#Bot token @Botmother
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
