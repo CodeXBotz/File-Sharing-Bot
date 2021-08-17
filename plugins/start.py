9b0e25fd36b11d67923feeb7b3b6fea44868d890
FILELANGKATELE <88260364+FILELANGKATELE@users.noreply.github.com>
2021-08-17 22:22:40 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,18 +94,14 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>cara liat videonya
-• klik join channel dibawah
-• join ke channel yang diarahkan
-• klik link relog
-• klik start</b>"
+    text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>klik disini untuk relog bot</a></b>"
+        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
     except ValueError:
         pass
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("JOIN CHANNEL", url = client.invitelink)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
     await message.reply(
         text = text,
         reply_markup = reply_markup,
