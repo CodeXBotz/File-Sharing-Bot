d01b91b9137b8f51f98e66e51808f7edafe999bd
Shabin-k <73272797+SHABIN-K@users.noreply.github.com>
2021-10-07 19:57:50 +0530

    Update sql.py

--- database/sql.py
+++ database/sql.py
@@ -4,11 +4,11 @@ import threading
 from sqlalchemy import create_engine
 from sqlalchemy import Column, TEXT, Numeric
 from sqlalchemy.ext.declarative import declarative_base
-from configs import Config
+from config import DB_URI
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 def start() -> scoped_session:
-    engine = create_engine(Config.DB_URI, client_encoding="utf8")
+    engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
