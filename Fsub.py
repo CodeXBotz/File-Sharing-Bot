f649b8fc4952dcbb45142f5db813fb530e4ec15e
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2021-10-12 12:55:57 +0530

    Update Fsub.py

--- Fsub.py
+++ Fsub.py
@@ -19,7 +19,7 @@ async def ForceSub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/Movies_request0).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/TMKOC_ALL_NEW_EPISODE).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -44,7 +44,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/Movies_request0).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/TMKOC_ALL_NEW_EPISODE).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
