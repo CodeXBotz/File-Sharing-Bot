0ae6a6deef4ff7f82d1fc5504bd2c29a0173695f
Shabin-k <73272797+SHABIN-K@users.noreply.github.com>
2021-10-19 20:07:31 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -145,9 +145,9 @@ async def not_joined(client: Client, message: Message):
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
-    msg = await client.send_message(chat_id=message.chat.id, text="`Counting Users....`")
+    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users = await full_userbase()
-    await msg.edit(f"{len(users)} **users are using this bot**")
+    await msg.edit(f"{len(users)} users are using this bot")
 
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
