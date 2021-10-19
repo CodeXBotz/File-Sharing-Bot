050645f23c36332e7b9e35d5eba2cdc028cc5d0c
Shabin-k <73272797+SHABIN-K@users.noreply.github.com>
2021-10-19 09:15:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -20,6 +20,9 @@ WAIT_MSG = """"<b>Processing ...</b>"""
 
 REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
 
+USERS_LIST = "<b>⭕️Total:</b>\n\n⭕️Subscribers - {}\n⭕️Blocked- {}"
+
+
 
 #=====================================================================================##
 
