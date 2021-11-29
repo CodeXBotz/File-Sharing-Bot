10cad5f1140ff40d837f55a64f60fe5d7008e73c
iamviratkohli <87019143+iamviratkohli@users.noreply.github.com>
2021-11-29 14:58:22 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,9 +8,9 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "about":
+    if data == "about 💟":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel  : <a href='https://t.me/Drey_Area'>Click here</a>\n○ Channel : @DreyArea\n○ Group : @Drey_Discuss</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
