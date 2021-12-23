5dfef0d0b79b631c41d9c1850279ffbac7b46a51
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2021-12-23 20:29:00 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,4 @@
-#(©)Codexbotz
+#(©)moviedhamaka11
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href=''>ಮೆಂಟಲ್</a>\n○ Language : <code>ಕನ್ನಡ</code>\n○ Library : <a href=''>Playit Backup</a>\n○ Source Code : <a href=''>ಬೇಕಾ ನಿಂಗೆ</a>\n○ Channel : @moviedhamaka11\n○ Support Group : @moviedhamaka11group</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
