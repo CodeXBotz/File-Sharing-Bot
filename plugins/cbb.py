78abce8225b49584cc781fdb6a03fd7194f686d1
AdwaithVarma2006 <92534654+AdwaithVarma2006@users.noreply.github.com>
2022-02-20 08:55:54 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Developer : <a href='tg://user?id={OWNER_ID}'>Var🌙Marvel</a>\n○Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/VarMarvel'>Click here</a>\n○ Channel : @DevilsAnus1\n○ Support Group : @Telegram</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
