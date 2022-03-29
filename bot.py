c198fe3426651c25b03b42f541808e8b8cb1b489
Leonardo Da Vinci <84631921+leanardo7994@users.noreply.github.com>
2022-03-29 16:23:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾𝙤𝙙𝙚 𝕏 𝘽𝙤𝙩𝙯\nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝐌𝐎𝐕𝐈𝐄𝐒 𝐄𝐌𝐏𝐎𝐑𝐈𝐎 ™\nhttps://t.me/movies_emperio")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
