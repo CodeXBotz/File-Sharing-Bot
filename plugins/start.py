bd575f3763a168dc333025747860517bcc1b12c7
Leonardo Da Vinci <84631921+leanardo7994@users.noreply.github.com>
2022-04-09 18:52:45 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -111,7 +111,7 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    mention = str(message.from_user.first_name)
+    mention = str(message.from_user.mention)
     text = f"<b>Hello {mention},</b>\n\nI Can Store 𝐌𝐎𝐕𝐈𝐄𝐒 𝐄𝐌𝐏𝐎𝐑𝐈𝐎 Files In This Bot And Other Users Can Access It From Special Link 📎\n\n<b><a href='https://t.me/movies_emperio'>YOU NEED TO JOIN IN OUR CHANNEL TO DOWNLOAD THE MOVIE FILES 📂</a></b>"
 
     message_text = message.text
