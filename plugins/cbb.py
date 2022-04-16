cc7502c3f8a7e50494b12e3c94aa5487cd947040
jikkubot32 <93936137+jikkubot32@users.noreply.github.com>
2022-04-16 14:38:13 +0400

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -15,7 +15,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🌎 Channel", url=f'https://t.me/filmclub_channel{link}'),
+                        InlineKeyboardButton("🌎 Channel", url='https://t.me/FilmClub_Channel')
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
                 ]
