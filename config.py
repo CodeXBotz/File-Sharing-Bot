aebb1fa73daac5f7c181e3767aa93e2e8f6d464d
vokepvideo <108939273+vokepvideo@users.noreply.github.com>
2022-08-19 04:14:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -22,6 +22,13 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
+FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "0"))
+FORCE_SUB_CHANNEL5 = int(os.environ.get("FORCE_SUB_CHANNEL5", "0"))
+FORCE_SUB_CHANNEL6 = int(os.environ.get("FORCE_SUB_CHANNEL6", "0"))
+FORCE_SUB_CHANNEL7 = int(os.environ.get("FORCE_SUB_CHANNEL7", "0"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
