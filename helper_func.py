57d024ff7b87345bfb04532b784ff93b4e4f1e9f
vokepvideo <108939273+vokepvideo@users.noreply.github.com>
2022-08-19 04:19:04 +0800

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -5,18 +5,39 @@ import re
 import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
-from config import FORCE_SUB_CHANNEL, ADMINS
+from config import FORCE_SUB_CHANNEL, ADMINS, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, FORCE_SUB_CHANNEL4, FORCE_SUB_CHANNEL5, FORCE_SUB_CHANNEL6, FORCE_SUB_CHANNEL7, FORCE_SUB_GROUP
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
         return True
+    if not FORCE_SUB_CHANNEL2:
+        return True
+    if not FORCE_SUB_CHANNEL3:
+        return True
+    if not FORCE_SUB_CHANNEL4:
+        return True
+    if not FORCE_SUB_CHANNEL5:
+        return True
+    if not FORCE_SUB_CHANNEL6:
+        return True
+    if not FORCE_SUB_CHANNEL7:
+        return True
+    if not FORCE_SUB_GROUP:
+        return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True
     try:
         member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL2, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL3, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL4, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL5, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL6, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL7, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_GROUP, user_id = user_id)
     except UserNotParticipant:
         return False
 
