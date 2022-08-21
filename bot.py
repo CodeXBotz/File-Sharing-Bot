bda4849c37eeba8d098bacd113003f004241ed16
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-08-21 17:34:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/TeamNexus")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/TeamNexusX")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
