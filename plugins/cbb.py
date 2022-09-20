b342abab0c771c7f3a544fce75527cd90d14cdd4
jikkubot32 <93936137+jikkubot32@users.noreply.github.com>
2022-09-20 11:55:50 +0400

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : @MalluBhai\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/kurachkanjiedukkatte/8'>Click here</a>\n○ Channel : @FilmClub_Channel\n○ Group : @FilmClubGroup</b>",
+            text = f"<b>○ Creator : @MalluBhai\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/kurachkanjiedukkatte/8'>Click here</a>\n○ Channel : @Film_ClubChannel\n○ Group : @Film_ClubGroup</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
