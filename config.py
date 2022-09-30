d6d910af03c7ba079ab8c4b2cf76486aa655d6c4
zonabansel <114482374+zonabansel@users.noreply.github.com>
2022-09-30 15:15:51 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5671251738:AAEbu6j7PXohij75EWQ4JKmRPxzE3DTwmf8")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "18164068"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "48b5398b5c58a15d7dfcdff146dfb0a4")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "ZonabanselBot"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "ZonabanselBot"))
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
