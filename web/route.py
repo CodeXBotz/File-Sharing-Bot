387602a4343d9cb2c6a317ab5ec1da5c10170712
ryme <88014043+rymmee@users.noreply.github.com>
2022-11-05 19:33:35 +0530

    Update route.py

--- web/route.py
+++ web/route.py
@@ -5,4 +5,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Status":"Running")
+    return web.json_response("CodeXBotz")
