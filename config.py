acf954c927205ce7d6b0e3bd8ac84e8c6017ff68
jinkomedi <119667213+jinkomedi@users.noreply.github.com>
2022-12-02 17:02:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first} & Welcome To [TIF] Links Bot </b> 📌Join our other Channels </b> @TIF_TvSeries1 🌹 @TIF_WebSeries </b> @TIF_Moviez 🌹 @TIF_Anime </b>  📌Our Discussion Group </b>  @TIFDiscuss 🌹 @TIF_OTs 🌹 @TIFRequests </b> 📌Our Official Bot  @TIFManagerBot </b> 📌Our Store </b> @TIF_Shoppie 🌹 @TIF_Vouches </b> 📌 A new initiative by [TIF], We are now on ENEBA.COM as </b> www.eneba.com/vendor/tif-gaming . </b> 📌 Follow us on Instagram </b> https://instagram.com/tif_network?igshid=YmMyMTA2M2Y= ")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first} & Welcome To [TIF] Links Bot <br> 📌Join our other Channels <br> @TIF_TvSeries1 🌹 @TIF_WebSeries <br> @TIF_Moviez 🌹 @TIF_Anime <br>  📌Our Discussion Group <br>  @TIFDiscuss 🌹 @TIF_OTs 🌹 @TIFRequests <br> 📌Our Official Bot  @TIFManagerBot <br> 📌Our Store <br> @TIF_Shoppie 🌹 @TIF_Vouches <br> 📌 A new initiative by [TIF], We are now on ENEBA.COM as <br> www.eneba.com/vendor/tif-gaming . <br> 📌 Follow us on Instagram <br> https://instagram.com/tif_network?igshid=YmMyMTA2M2Y= ")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
