04d1c73d0b72400b084486a6f7c4fcd671a7d3a2
dor3Monbotz <118621248+dor3Monbotz@users.noreply.github.com>
2023-01-03 21:56:18 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "⭐about⭐":
+    if data == "about":
         await query.message.edit_text(
             text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/dor3Monbotz/crownFileShearBot'>Click here</a>\n○ Channel : https://t.me/projectcrown\n○ Support Group : https://t.me/crownbotzsupport</b>",
             disable_web_page_preview = True,
