e78b33d51aacc0dcea76cf5cadb6869a1c348e60
dor3Monbotz <118621248+dor3Monbotz@users.noreply.github.com>
2023-01-03 22:07:11 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,14 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/dor3Monbotz/crownFileShearBot'>Click here</a>\n○ Channel : https://t.me/projectcrown\n○ Support Group : https://t.me/crownbotzsupport</b>",
+            text = f"""
+╭━━━━━━━━━━━━━━━➣
+┣⪼👑 Creator : <a href='tg://user?id={OWNER_ID}'>Pro Owner</a>
+┣⪼👨‍💻 Language : <code>Python3</code>
+┣⪼✏️ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {version}</a>
+┣⪼🌀 Source Code : <a href='https://github.com/dor3Monbotz/crownFileShearBot'>Click here</a>
+┣⪼📕 Channel : <a href= "https://t.me/Cinecoder">@Cinecoder</a>
+╰━━━━━━━━━━━━━━━➣""",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
