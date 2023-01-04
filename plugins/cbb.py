a6394d65384724bb802e7dcd5dacc2788576eb88
dor3Monbotz <118621248+dor3Monbotz@users.noreply.github.com>
2023-01-04 17:12:20 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,13 +11,17 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     if data == "about":
         await query.message.edit_text(
             text = f""" \n
+😋𝙸 𝙰𝙼 𝙰 𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻 𝚂𝚃𝙾𝚁𝙰𝙶𝙴 𝙱𝙾𝚃😋
+     𝙸 𝙰𝙼 𝙿𝙰𝚁𝚃 𝙾𝙵 @Cinecoder
+🤤🥴𝙳𝚁𝙴𝙰𝙼𝙻𝙰𝙽𝙳 𝙾𝙵 𝙿𝚄𝙱𝙶 𝙲𝙾𝙽𝙵𝙸𝙶🥴🤤
 ╭━━━━━━━━━━━━━━━➣
-┣⪼👑 Creator : <a href='tg://user?id={OWNER_ID}'>Pro Owner</a>
-┣⪼👨‍💻 Language : <code>Python3</code>
-┣⪼✏️ Library : <a href='https://docs.pyrogram.org/'>Pyrogram</a>
-┣⪼🌀 Source Code : <a href='https://github.com/dor3Monbotz/crownFileShearBot'>Click here</a>
-┣⪼📕 Channel : <a href= "https://t.me/Cinecoder">@Cinecoder</a>
-╰━━━━━━━━━━━━━━━➣""",
+[❤‍🔥]➠👑𝙲𝚁𝙴𝙰𝚃𝙾𝚁👑 : <a href='tg://user?id={OWNER_ID}'>𝙽𝙾𝙾𝙱 </a>
+[❤‍🔥]➠👨‍💻𝙻𝚊𝚗𝚐𝚞𝚊𝚐𝚎👨‍💻: <a herf='https://python3.com'>𝙿𝚈𝚃𝙷𝙾𝙽3</a>
+[❤‍🔥]➠🎮𝙲𝙷𝙰𝙽𝙽𝙴𝙻🎮 : <a herf='https://t.me/Cinecoder'>@𝙲𝙸𝙽𝙴𝙲𝙾𝙳𝙴𝚁</a>
+[❤‍🔥]➠✏️𝙻𝙸𝙱𝚁𝙰𝚁𝚈✏️ : <a href='https://docs.pyrogram.org/'>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+╰━━━━━━━━━━━━━━━➣
+𝚆𝙰𝙽𝚃 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽 𝙱𝙾𝚃 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 @GaikwadKunal
+""",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
