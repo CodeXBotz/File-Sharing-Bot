7b7efbbe57555d1905615413ef358e9ee9d2af94
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2023-12-22 16:02:22 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : Private \n○ Channel : @geniflix</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : Private \n○ Channel : @irish_family</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
