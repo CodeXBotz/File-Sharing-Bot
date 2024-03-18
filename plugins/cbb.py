fae8f1541e9eab47f5bde51e7cf93a83c05ae7c2
INDIAHACKER_ICS <140348179+indiahackercyber@users.noreply.github.com>
2024-03-18 11:22:05 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>@Sksksk99</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='@SKsksk99'>Click here</a>\n○ Channel : @INDIAHACKER_ICS\n○ Hacking and course : @INDIAHACKER_ICS</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
