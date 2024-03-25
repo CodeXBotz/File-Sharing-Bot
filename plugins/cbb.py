4e0bc9d731a687f0b14158779d1eee50459d060f
yujiitadori230 <164171521+yujiitadori230@users.noreply.github.com>
2024-03-25 17:14:17 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Website : <a href='https://dramaflixtamilofficial.blogspot.com'>Click here</a>\n○ Channel : @ck_Drama_Tamil_Dubbed\n○ Support Group : @DramaFlixTamil</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
